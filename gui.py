2ca37ae939203b4e5395318869d3d765c8523dd7
Benjamin Kasavan <bkasavan@gmail.com>
2020-02-16 21:49:37 -0500

    added my identidty

--- gui.py
+++ gui.py
@@ -22,13 +22,15 @@ fileName = "data.csv" #make sure there's a txt file called data.txt in the same 
 
 def submit(*args): #this is the function that takes all the submitted data and slips it into the csv file
     fullname = name.get()
+    fullemail = email.get()
     reason = purpose.get()
     now = datetime.now()
     timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
-    dat=[fullname, reason, timestamp]
+    dat=[" " + fullname, " " +  reason, " " + fullemail, " " +  timestamp]
     with open('data.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(dat)
+    #root.name_entry.delete(0, 'end')
     messagebox.showinfo(message='You\'ve been signed in!' )
 
     
@@ -47,26 +49,29 @@ root.columnconfigure(0, weight = 1)
 root.rowconfigure(0, weight=1)
 
 
-
-
 name = StringVar()
 purpose = StringVar()
 purpose.set("use")
+email = StringVar()
 
 name_entry = ttk.Entry(mainframe, width=7, textvariable=name)
 name_entry.grid(column=2, row=2, sticky=(W, E))
 
 
 #ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Sign In", command=submit).grid(column=2, row=5, sticky=W)
+ttk.Button(mainframe, text="Sign In", command=submit).grid(column=2, row=7, sticky=W)
 
-ttk.Label(mainframe, text="Please sign in below whenever you visit SOPO").grid(column=1, row=1, sticky=W, columnspan=3)
+ttk.Label(mainframe, text="Please sign in below whenever you visit SOPO").grid(column=1, row=1, sticky=W, columnspan=4)
 ttk.Label(mainframe, text="Name: ").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="What is your main reason for coming today?").grid(column=1, row=3, sticky=W, columnspan=3)
+ttk.Label(mainframe, text="What is your main reason for coming today?").grid(column=1, row=3, sticky=W, columnspan=4)
 repair = ttk.Radiobutton(mainframe, text='Repairing my bike', variable=purpose, value='use').grid(column=1, row=4)
 donate = ttk.Radiobutton(mainframe, text='Dropping off donation', variable=purpose, value='donate').grid(column=2, row=4)
 volunteer = ttk.Radiobutton(mainframe, text='Volunteering', variable=purpose, value='volunteer').grid(column=3, row=4)
 other = ttk.Radiobutton(mainframe, text='Other', variable=purpose, value='other').grid(column=4, row=4)
+ttk.Label(mainframe, text="If you want to be added to our monthly email, give us information below!").grid(column=1, row=5, sticky=W, columnspan=4)
+ttk.Label(mainframe, text="Email: ").grid(column=1, row=6, sticky=E)
+email_entry = ttk.Entry(mainframe, width=7, textvariable=email)
+email_entry.grid(column=2, row=6, sticky=(W, E))
 
 #ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
